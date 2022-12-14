ac764e1417a3f633ebc92b59fed10af724623de8
ruan.vieira <ruan.vieira@UNIFESSPA.LOCAL>
2022-12-14 12:44:00 -0300

    add codes

---
+++ java/First/testeConta.java
@@ -0,0 +1,9 @@
+package java.First;
+
+public class testeConta {
+    public static void main(String[] args) {
+        System.out.println("Km");
+        Carro gol = new Carro();
+        gol.ligar();
+    }
+}
